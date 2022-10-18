d3018b2fb6330c0fce8896aa2ea27dfb60594e42
Luka Foy <107006746+popsnot@users.noreply.github.com>
2022-10-19 09:25:25 +1300

    Update rnv_script.py
    script that hashes webpage content, then compares it to the previous hash to see if any changes have been made. if changes have been made, it sends the user (or whoever) an email.
    
    can be applied to any website you wanna monitor

--- rnv_script.py
+++ rnv_script.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen, Request
 import win32com.client
  
 outlook = win32com.client.Dispatch('outlook.application')
-url = Request('https://moshtix.co.nz/v2/event/rhythm-and-vines-2022-the-20th-anniversary/123507?skin=RVD22',
+url = Request('your webpage here',
               headers={'User-Agent': 'Mozilla/5.0'})
  
 response = urlopen(url).read()
@@ -25,15 +25,9 @@ while True:
         else:
             print("New tickets up")
             mail = outlook.CreateItem(0)
-            mail.To = 'lfo47@uclive.ac.nz'
-            mail.Subject = 'New RNV Resale Tickets'
-            mail.Body = "New tickets got added \n https://moshtix.co.nz/v2/event/rhythm-and-vines-2022-the-20th-anniversary/123507?skin=RVD22"
-            mail.Send()
-            
-            mail = outlook.CreateItem(0)
-            mail.To = 'evafgalvin@gmail.com'
-            mail.Subject = 'New RNV Resale Tickets'
-            mail.Body = "New tickets got added \n https://moshtix.co.nz/v2/event/rhythm-and-vines-2022-the-20th-anniversary/123507?skin=RVD22"
+            mail.To = '***insert your email here***'
+            mail.Subject = '***'
+            mail.Body = "***"
             mail.Send()
            
             response = urlopen(url).read()
